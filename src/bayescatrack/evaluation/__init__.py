a01574fb31734ba1aca7259175886d8a1bdc73e6
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-26 00:31:07 +0200

    Centralize evaluation exports

--- src/bayescatrack/evaluation/__init__.py
+++ src/bayescatrack/evaluation/__init__.py
@@ -1,6 +1,7 @@
 """Evaluation helpers for BayesCaTrack benchmarks."""
 
 from .complete_track_scores import (
+    __all__ as _COMPLETE_TRACK_SCORE_EXPORTS,
     complete_track_set,
     normalize_track_matrix,
     pairwise_track_set,
@@ -12,14 +13,4 @@ from .complete_track_scores import (
 )
 from .track2p_metrics import score_track_matrix_against_reference
 
-__all__ = [
-    "complete_track_set",
-    "normalize_track_matrix",
-    "pairwise_track_set",
-    "score_complete_tracks",
-    "score_pairwise_tracks",
-    "score_track_matrices",
-    "score_track_matrix_against_reference",
-    "summarize_tracks",
-    "track_lengths",
-]
+__all__ = [*_COMPLETE_TRACK_SCORE_EXPORTS, "score_track_matrix_against_reference"]
