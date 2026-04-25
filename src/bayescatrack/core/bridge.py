795e4e4210dd37e39806274fcc9ef9c1d8bebf24
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-25 11:30:53 +0200

    Avoid duplicate-code export list in bridge wrapper

--- src/bayescatrack/core/bridge.py
+++ src/bayescatrack/core/bridge.py
@@ -1,19 +1,25 @@
 """Public bridge exports for BayesCaTrack core."""
 
+# pylint: disable=duplicate-code
+
 from .._exports import BRIDGE_PUBLIC_NAMES
-from ._bridge_impl import (
-    CalciumPlaneData,
-    SessionAssociationBundle,
-    Track2pSession,
-    build_consecutive_session_association_bundles,
-    build_session_pair_association_bundle,
-    export_subject_to_npz,
-    find_track2p_session_dirs,
-    load_raw_npy_plane,
-    load_suite2p_plane,
-    load_track2p_subject,
-    main,
-    summarize_subject,
+from . import _bridge_impl
+
+CalciumPlaneData = _bridge_impl.CalciumPlaneData
+SessionAssociationBundle = _bridge_impl.SessionAssociationBundle
+Track2pSession = _bridge_impl.Track2pSession
+build_consecutive_session_association_bundles = (
+    _bridge_impl.build_consecutive_session_association_bundles
 )
+build_session_pair_association_bundle = (
+    _bridge_impl.build_session_pair_association_bundle
+)
+export_subject_to_npz = _bridge_impl.export_subject_to_npz
+find_track2p_session_dirs = _bridge_impl.find_track2p_session_dirs
+load_raw_npy_plane = _bridge_impl.load_raw_npy_plane
+load_suite2p_plane = _bridge_impl.load_suite2p_plane
+load_track2p_subject = _bridge_impl.load_track2p_subject
+main = _bridge_impl.main
+summarize_subject = _bridge_impl.summarize_subject
 
 __all__ = BRIDGE_PUBLIC_NAMES
