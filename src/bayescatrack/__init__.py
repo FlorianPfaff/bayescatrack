2c49ac8bfc86c1a5d18142565898f2e451b79b19
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-25 11:28:54 +0200

    Make bayescatrack public exports explicit

--- src/bayescatrack/__init__.py
+++ src/bayescatrack/__init__.py
@@ -1,6 +1,20 @@
 """BayesCaTrack public package API."""
 
-from . import core as _core
-from ._exports import reexport
+from .core import bridge as _bridge
 
-__all__ = reexport(_core, globals())
+CalciumPlaneData = _bridge.CalciumPlaneData
+SessionAssociationBundle = _bridge.SessionAssociationBundle
+Track2pSession = _bridge.Track2pSession
+build_consecutive_session_association_bundles = (
+    _bridge.build_consecutive_session_association_bundles
+)
+build_session_pair_association_bundle = _bridge.build_session_pair_association_bundle
+export_subject_to_npz = _bridge.export_subject_to_npz
+find_track2p_session_dirs = _bridge.find_track2p_session_dirs
+load_raw_npy_plane = _bridge.load_raw_npy_plane
+load_suite2p_plane = _bridge.load_suite2p_plane
+load_track2p_subject = _bridge.load_track2p_subject
+main = _bridge.main
+summarize_subject = _bridge.summarize_subject
+
+__all__ = _bridge.__all__
