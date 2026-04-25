154aea5a5e7ce25d013ceac504922a1116d9d91f
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-25 11:29:04 +0200

    Make core bridge exports explicit

--- src/bayescatrack/core/bridge.py
+++ src/bayescatrack/core/bridge.py
@@ -1,6 +1,19 @@
 """Public bridge exports for BayesCaTrack core."""
 
-from .._exports import BRIDGE_PUBLIC_NAMES, reexport
-from . import _bridge_impl
+from .._exports import BRIDGE_PUBLIC_NAMES
+from ._bridge_impl import (
+    CalciumPlaneData,
+    SessionAssociationBundle,
+    Track2pSession,
+    build_consecutive_session_association_bundles,
+    build_session_pair_association_bundle,
+    export_subject_to_npz,
+    find_track2p_session_dirs,
+    load_raw_npy_plane,
+    load_suite2p_plane,
+    load_track2p_subject,
+    main,
+    summarize_subject,
+)
 
-__all__ = reexport(_bridge_impl, globals(), BRIDGE_PUBLIC_NAMES)
+__all__ = BRIDGE_PUBLIC_NAMES
