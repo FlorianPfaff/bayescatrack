31d20631df37dd7541ab04600a75ff89097dc940
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-01 15:06:19 +0200

    Export calibration diagnostics

--- src/bayescatrack/evaluation/__init__.py
+++ src/bayescatrack/evaluation/__init__.py
@@ -1,11 +1,17 @@
 """Evaluation helpers for BayesCaTrack benchmarks."""
 
+from . import calibration as _calibration
 from . import complete_track_scores as _scores
 from . import track2p_metrics as _track2p_metrics
 
+brier_score = _calibration.brier_score
+calibration_bin_table = _calibration.calibration_bin_table
 complete_track_set = _scores.complete_track_set
+expected_calibration_error = _calibration.expected_calibration_error
+maximum_calibration_error = _calibration.maximum_calibration_error
 normalize_track_matrix = _scores.normalize_track_matrix
 pairwise_track_set = _scores.pairwise_track_set
+score_binary_calibration = _calibration.score_binary_calibration
 score_complete_tracks = _scores.score_complete_tracks
 score_false_continuations = _scores.score_false_continuations
 score_pairwise_tracks = _scores.score_pairwise_tracks
@@ -14,4 +20,4 @@ summarize_tracks = _scores.summarize_tracks
 track_lengths = _scores.track_lengths
 score_track_matrix_against_reference = _track2p_metrics.score_track_matrix_against_reference
 
-__all__ = list(_scores.__all__) + ["score_track_matrix_against_reference"]
+__all__ = list(_scores.__all__) + list(_calibration.__all__) + ["score_track_matrix_against_reference"]
